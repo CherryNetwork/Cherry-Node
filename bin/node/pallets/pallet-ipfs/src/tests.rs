7bd8212ae1fd99f09feafab8561382fbd8a235ce
Charalampos Mitrodimas <charmitro@pm.me>
2022-02-11 10:08:05 +0200

    add: delete_ipfs_asset tests

--- bin/node/pallets/pallet-ipfs/src/tests.rs
+++ bin/node/pallets/pallet-ipfs/src/tests.rs
@@ -1,7 +1,7 @@
 use super::*;
-use frame_support::assert_ok;
+use frame_support::{assert_noop, assert_ok};
 use mock::*;
-use sp_core::offchain::{testing, IpfsResponse, OffchainDbExt, OffchainWorkerExt, TransactionPoolExt};
+use sp_core::offchain::{testing, IpfsResponse, OffchainWorkerExt, TransactionPoolExt};
 use sp_core::Pair;
 use sp_keystore::{testing::KeyStore, KeystoreExt, SyncCryptoStore};
 use std::sync::Arc;
@@ -25,13 +25,11 @@ fn cherry_ipfs_add_bytes_works_for_valid_value() {
 			.to_vec();
 	let cid_vec = "QmPZv7P8nQUSh2CpqTvUeYemFyjvMjgWEs8H1Tm8b3zAm9".as_bytes().to_vec();
 	let size = 1024;
-	let reserve_price = 100;
 
 	let expected_data_command = crate::DataCommand::AddBytes(
 		OpaqueMultiaddr(multiaddr_vec.clone()),
 		cid_vec.clone(),
 		size.clone(),
-		reserve_price.clone(),
 		p.clone().public(),
 		true,
 	);
@@ -42,7 +40,6 @@ fn cherry_ipfs_add_bytes_works_for_valid_value() {
 			multiaddr_vec.clone(),
 			cid_vec.clone(),
 			size.clone(),
-			reserve_price.clone(),
 		));
 
 		let mut data_queue = crate::DataQueue::<Test>::get();
@@ -79,7 +76,6 @@ fn cherry_can_add_bytes_to_ipfs() {
 			.to_vec();
 	let cid_vec = "QmPZv7P8nQUSh2CpqTvUeYemFyjvMjgWEs8H1Tm8b3zAm9".as_bytes().to_vec();
 	let size = 1024;
-	let reserve_price = 100;
 	let bytes = "sjdasdadasdjasdlasd".as_bytes().to_vec();
 
 	// mock IPFS calls
@@ -119,7 +115,7 @@ fn cherry_can_add_bytes_to_ipfs() {
 			response: Some(IpfsResponse::Success),
 			..Default::default()
 		});
-}
+	}
 
 	t.execute_with(|| {
 		assert_ok!(mock::Ipfs::create_ipfs_asset(
@@ -127,7 +123,6 @@ fn cherry_can_add_bytes_to_ipfs() {
 			multiaddr_vec.clone(),
 			cid_vec.clone(),
 			size.clone(),
-			reserve_price.clone(),
 		));
 
 		assert_ok!(mock::Ipfs::submit_ipfs_add_results(
@@ -135,8 +130,174 @@ fn cherry_can_add_bytes_to_ipfs() {
 			p.clone().public(),
 			cid_vec.clone(),
 			size.clone(),
-			reserve_price.clone(),
 		));
+
+		assert_ok!(mock::Ipfs::handle_data_requests());
+	});
+}
+
+#[test]
+fn cherry_can_delete_from_ipfs() {
+	let (p, _) = sp_core::sr25519::Pair::generate();
+	let (offchain, state) = testing::TestOffchainExt::new();
+	let (pool, _) = testing::TestTransactionPoolExt::new();
+	const PHRASE: &str =
+		"news slush supreme milk chapter athlete soap sausage put clutch what kitten";
+	let keystore = KeyStore::new();
+	SyncCryptoStore::sr25519_generate_new(
+		&keystore,
+		crate::KEY_TYPE,
+		Some(&format!("{}/hunter1", PHRASE)),
+	)
+	.unwrap();
+
+	let mut t = new_test_ext_funded(p.clone());
+	t.register_extension(OffchainWorkerExt::new(offchain));
+	t.register_extension(TransactionPoolExt::new(pool));
+	t.register_extension(KeystoreExt(Arc::new(keystore)));
+
+	let multiaddr_vec =
+		"/ip4/127.0.0.1/tcp/4001/p2p/12D3KooWJdTrFkpPFMi4112oJUKeNvU4Haskg2rqNRCjdgc1yyVH"
+			.as_bytes()
+			.to_vec();
+	let cid_vec = "QmPZv7P8nQUSh2CpqTvUeYemFyjvMjgWEs8H1Tm8b3zAm9".as_bytes().to_vec();
+	let size = 1024;
+	let bytes = "sjdasdadasdjasdlasd".as_bytes().to_vec();
+
+	// mock IPFS calls
+	// These have to represent exactly the same ipfs_requests that
+	// are done in your actual code and wit the same order.
+	// - @charmitro
+	{
+		let mut state = state.write();
+		// connect to external node
+		state.expect_ipfs_request(testing::IpfsPendingRequest {
+			response: Some(IpfsResponse::Success),
+			..Default::default()
+		});
+		// fetch data
+		state.expect_ipfs_request(testing::IpfsPendingRequest {
+			id: sp_core::offchain::IpfsRequestId(0),
+			response: Some(IpfsResponse::CatBytes(bytes.clone())),
+			..Default::default()
+		});
+		// disconnect from the external node
+		state.expect_ipfs_request(testing::IpfsPendingRequest {
+			response: Some(IpfsResponse::Success),
+			..Default::default()
+		});
+		// add bytes to your local node
+		state.expect_ipfs_request(testing::IpfsPendingRequest {
+			response: Some(IpfsResponse::AddBytes(cid_vec.clone())),
+			..Default::default()
+		});
+		// insert pin
+		state.expect_ipfs_request(testing::IpfsPendingRequest {
+			response: Some(IpfsResponse::Success),
+			..Default::default()
+		});
+		// disconnect
+		state.expect_ipfs_request(testing::IpfsPendingRequest {
+			response: Some(IpfsResponse::Success),
+			..Default::default()
+		});
+	}
+
+	t.execute_with(|| {
+		assert_ok!(mock::Ipfs::create_ipfs_asset(
+			Origin::signed(p.clone().public()),
+			multiaddr_vec.clone(),
+			cid_vec.clone(),
+			size.clone(),
+		));
+
+		assert_ok!(mock::Ipfs::submit_ipfs_add_results(
+			Origin::signed(p.clone().public()),
+			p.clone().public(),
+			cid_vec.clone(),
+			size.clone(),
+		));
+
+		assert_ok!(mock::Ipfs::handle_data_requests());
+	});
+
+	{
+		let mut state = state.write();
+		// connect to external node
+		state.expect_ipfs_request(testing::IpfsPendingRequest {
+			response: Some(IpfsResponse::RemoveBlock(cid_vec.clone())),
+			..Default::default()
+		});
+	}
+
+	t.execute_with(|| {
+		assert_ok!(mock::Ipfs::delete_ipfs_asset(
+			Origin::signed(p.clone().public()),
+			multiaddr_vec.clone(),
+			cid_vec.clone(),
+		));
+
+		assert_ok!(mock::Ipfs::submit_ipfs_delete_results(
+			Origin::signed(p.clone().public()),
+			cid_vec.clone(),
+		));
+
+		assert_ok!(mock::Ipfs::handle_data_requests());
+	});
+}
+
+#[test]
+fn cherry_delete_ipfs_asset_ipfs_not_exist() {
+	let (p, _) = sp_core::sr25519::Pair::generate();
+	let (offchain, state) = testing::TestOffchainExt::new();
+	let (pool, _) = testing::TestTransactionPoolExt::new();
+	const PHRASE: &str =
+		"news slush supreme milk chapter athlete soap sausage put clutch what kitten";
+	let keystore = KeyStore::new();
+	SyncCryptoStore::sr25519_generate_new(
+		&keystore,
+		crate::KEY_TYPE,
+		Some(&format!("{}/hunter1", PHRASE)),
+	)
+	.unwrap();
+
+	let mut t = new_test_ext_funded(p.clone());
+	t.register_extension(OffchainWorkerExt::new(offchain));
+	t.register_extension(TransactionPoolExt::new(pool));
+	t.register_extension(KeystoreExt(Arc::new(keystore)));
+
+	let multiaddr_vec =
+		"/ip4/127.0.0.1/tcp/4001/p2p/12D3KooWJdTrFkpPFMi4112oJUKeNvU4Haskg2rqNRCjdgc1yyVH"
+			.as_bytes()
+			.to_vec();
+	let cid_vec = "QmPZv7P8nQUSh2CpqTvUeYemFyjvMjgWEs8H1Tm8b3zAm9".as_bytes().to_vec();
+
+	{
+		let mut state = state.write();
+		// connect to external node
+		state.expect_ipfs_request(testing::IpfsPendingRequest {
+			response: Some(IpfsResponse::RemoveBlock(cid_vec.clone())),
+			..Default::default()
+		});
+	}
+
+	t.execute_with(|| {
+		assert_noop!(
+			mock::Ipfs::delete_ipfs_asset(
+				Origin::signed(p.clone().public()),
+				multiaddr_vec.clone(),
+				cid_vec.clone(),
+			),
+			Error::<Test>::IpfsNotExist
+		);
+
+		assert_noop!(
+			mock::Ipfs::submit_ipfs_delete_results(
+				Origin::signed(p.clone().public()),
+				cid_vec.clone(),
+			),
+			Error::<Test>::IpfsNotExist
+		);
 
 		assert_ok!(mock::Ipfs::handle_data_requests());
 	});
