c629b819ed0243b280e3a42f0282878b3edf0243
Chubak <chubakbidpaa@gmail.com>
2022-04-06 00:06:32 +0430

    added: commented names of users next to their hashes

--- bin/node/cli/src/chain_spec.rs
+++ bin/node/cli/src/chain_spec.rs
@@ -356,10 +356,10 @@ pub fn testnet_genesis(
 		treasury: Default::default(),
 		updater: UpdaterConfig {
 			phantom: Default::default(),
-			members: vec![AccountId::from_ss58check("5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY").unwrap(),
-						AccountId::from_ss58check("5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty").unwrap(),
-						AccountId::from_ss58check("5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y").unwrap(),	
-						AccountId::from_ss58check("5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy").unwrap(),		],
+			members: vec![AccountId::from_ss58check("5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY").unwrap() /// ALICE,
+						AccountId::from_ss58check("5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty").unwrap() /// BOB,
+						AccountId::from_ss58check("5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y").unwrap() /// CHARLIE,	
+						AccountId::from_ss58check("5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy").unwrap() /// DAVE,		],
 		},
 		society: SocietyConfig {
 			members: endowed_accounts
