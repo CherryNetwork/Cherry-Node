0ee73f29039759c049aabf9e4f6759ae78dbf33d
Charalampos Mitrodimas <charmitro@pm.me>
2022-05-03 17:15:00 +0300

    cargo fmt

--- frame/collective/src/lib.rs
+++ frame/collective/src/lib.rs
@@ -597,7 +597,7 @@ pub mod pallet {
 				if position_yes.is_none() {
 					voting.ayes.push(who.clone());
 				} else {
-					return Err(Error::<T, I>::DuplicateVote.into());
+					return Err(Error::<T, I>::DuplicateVote.into())
 				}
 				if let Some(pos) = position_no {
 					voting.nays.swap_remove(pos);
@@ -606,7 +606,7 @@ pub mod pallet {
 				if position_no.is_none() {
 					voting.nays.push(who.clone());
 				} else {
-					return Err(Error::<T, I>::DuplicateVote.into());
+					return Err(Error::<T, I>::DuplicateVote.into())
 				}
 				if let Some(pos) = position_yes {
 					voting.ayes.swap_remove(pos);
@@ -706,7 +706,7 @@ pub mod pallet {
 					),
 					Pays::Yes,
 				)
-					.into());
+					.into())
 			} else if disapproved {
 				Self::deposit_event(Event::Closed(proposal_hash, yes_votes, no_votes));
 				let proposal_count = Self::do_disapprove_proposal(proposal_hash);
@@ -714,7 +714,7 @@ pub mod pallet {
 					Some(T::WeightInfo::close_early_disapproved(seats, proposal_count)),
 					Pays::No,
 				)
-					.into());
+					.into())
 			}
 
 			// Only allow actual closing of the proposal after the voting period has ended.
