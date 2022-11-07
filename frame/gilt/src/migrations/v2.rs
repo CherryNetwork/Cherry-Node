9f3e38be4c001428a9b0613166e797a6ab1c5291
Fotis Zikos <fzikos@gmail.com>
2022-11-07 21:28:40 +0200

    fix: initial storage migration value

--- frame/gilt/src/migrations/v2.rs
+++ frame/gilt/src/migrations/v2.rs
@@ -3,9 +3,10 @@ use frame_support::{
     traits::{
         Get, GetStorageVersion, StorageVersion, PalletInfoAccess, 
     },
-    weights::Weight,
+    weights::Weight, BoundedVec,
 };
 use sp_runtime::traits::Zero;
+use sp_std::vec;
 
 pub fn migrate<T: frame_system::Config + crate::Config, P: GetStorageVersion + PalletInfoAccess>() -> Weight {
     let on_chain_storage_version = <P as GetStorageVersion>::on_chain_storage_version();
@@ -16,7 +17,11 @@ pub fn migrate<T: frame_system::Config + crate::Config, P: GetStorageVersion + P
 	);
 
     if on_chain_storage_version < 4 {
-		QueueTotals::<T>::put(vec![(0, BalanceOf::<T>::zero()); T::QueueCount::get() as usize]);
+        let unbounded = vec![(0, BalanceOf::<T>::zero()); T::QueueCount::get() as usize];
+        let bounded: BoundedVec<_, _> = unbounded
+            .try_into()
+            .expect("QueueTotals should support up to QueueCount items. qed");
+        QueueTotals::<T>::put(bounded);
 		log_migration("migration");
 
 		StorageVersion::new(2).put::<P>();
