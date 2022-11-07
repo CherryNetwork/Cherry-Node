f9ef9a5e260e84193a3cd383699071cf6979fe3f
Fotis Zikos <fzikos@gmail.com>
2022-11-07 18:27:22 +0200

    add: storage migration for pallet_gilt initializing QueuesTotal storage

---
+++ frame/gilt/src/migrations/v2.rs
@@ -0,0 +1,40 @@
+use crate::*;
+use frame_support::{ 
+    traits::{
+        Get, GetStorageVersion, StorageVersion, PalletInfoAccess, 
+    },
+    weights::Weight,
+};
+use sp_runtime::traits::Zero;
+
+pub fn migrate<T: frame_system::Config + crate::Config, P: GetStorageVersion + PalletInfoAccess>() -> Weight {
+    let on_chain_storage_version = <P as GetStorageVersion>::on_chain_storage_version();
+	log::info!(
+		target: "runtime::collective",
+		"Running migration to v2 for collective with storage version {:?}",
+		on_chain_storage_version,
+	);
+
+    if on_chain_storage_version < 4 {
+		QueueTotals::<T>::put(vec![(0, BalanceOf::<T>::zero()); T::QueueCount::get() as usize]);
+		log_migration("migration");
+
+		StorageVersion::new(2).put::<P>();
+		<T as frame_system::Config>::BlockWeights::get().max_block
+	} else {
+		log::warn!(
+			target: "runtime::gilt",
+			"Attempted to apply migration to v2 but failed because storage version is {:?}",
+			on_chain_storage_version,
+		);
+		0
+	}
+}
+
+fn log_migration(stage: &str) {
+	log::info!(
+		target: "runtime::gilt",
+		"{}",
+		stage,
+	);
+}
